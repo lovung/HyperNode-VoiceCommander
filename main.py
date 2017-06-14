bc7feadbe8ef084e6b867ab3d2f0421d504ad23d
vulong <hoang.phuc@nxcomm.com>
2017-06-14 04:07:33 -0700

    Record and send to Watson API.

--- main.py
+++ main.py
@@ -12,9 +12,11 @@ print(json.dumps(speech_to_text.models(), indent=2))
 
 print(json.dumps(speech_to_text.get_model('en-US_BroadbandModel'), indent=2))
 
-with open(join(dirname(__file__), 'Whattimeisit.wav'),
+with open(join(dirname(__file__), 'demo.wav'),
           'rb') as audio_file:
     print(json.dumps(speech_to_text.recognize(
         audio_file, content_type='audio/wav', timestamps=True,
         word_confidence=True),
         indent=2))
+
+
